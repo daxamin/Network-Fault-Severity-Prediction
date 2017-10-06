c234ce01790183e01f6789509563a516c59d303f
Dax Amin <dkamin@ncsu.edu>
2017-10-06 12:51:06 -0400

    Adding project

---
+++ data_transform_merge.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from os.path import join
+import numpy as np
+    
+data_path = r'/Major 1/data/'
+
+# add neighbors features, predecessor and successor
+def featured_neighbors():
+
+    # propagate list forth or back by filling NaNs with predecessor value    
+    def fill(s, rev=False):
+        if rev: s = s[::-1]
+        filler = 0
+        for i,v in enumerate(s):
+            s[i] = filler = filler if np.isnan(v) else v
+        return s[::-1] if rev else s
+    
+    t = logFeature.reset_index().join(train, on='id').join(test, on='id',
+                    lsuffix='_train', rsuffix='_test').drop_duplicates('id')
+
+    t['x1'] = fill(t['fault_severity'].shift(1).values)
+    t['x2'] = fill(t['fault_severity'].shift(-1).values, rev=True)
+    
+    # 'position' - post competition addition
+    t['location'] = t[['location_train', 'location_test']].fillna(0).astype(int).sum(axis=1)
+    groups = t.groupby('location')
+    t['position'] = groups.cumcount() / groups['id'].transform(len)
+   
+    return t[['id', 'x1', 'x2', 'position']].set_index('id')
+    
+
+#combine all tables
+resourceType = pd.read_csv(join(data_path, 'resource_type.csv'), index_col=0)
+resourceTypeVectorized = pd.get_dummies(resourceType).groupby(resourceType.index).sum().astype(int)
+resourceTypeVectorized['resource_type_count'] = pd.get_dummies(resourceType).groupby(resourceType.index).size()
+
+eventType = pd.read_csv(join(data_path, 'event_type.csv'), index_col=0)
+eventTypeVectorized = pd.get_dummies(eventType).groupby(eventType.index).sum().astype(int)
+eventTypeVectorized['event_type_count'] = pd.get_dummies(eventType).groupby(eventType.index).size()
+
+logFeature = pd.read_csv(join(data_path, 'log_feature.csv'), index_col=0)
+logFeatureVectorized = pd.get_dummies(logFeature)
+logFeatureVectorized.iloc[:, 1:] = logFeatureVectorized.iloc[:, 1:].multiply(logFeatureVectorized['volume'], axis="index")
+logFeatureVectorized = logFeatureVectorized.groupby(logFeature.index).sum().astype(int)
+logFeatureVectorized['log_feature_count'] = pd.get_dummies(logFeature).groupby(logFeature.index).size()
+
+severityType = pd.read_csv(join(data_path, 'severity_type.csv'), index_col=0)
+severityType['severity_type'] = [l.replace('severity_type ', '') for l in severityType['severity_type']]
+
+train = pd.read_csv(join(data_path, 'train.csv'), index_col=0)
+train['location'] = [l.replace('location ', '') for l in train['location']]
+train_merged = train.join(resourceTypeVectorized).join(eventTypeVectorized).join(logFeatureVectorized).join(severityType)
+
+test = pd.read_csv(join(data_path, 'test.csv'), index_col=0)
+test['location'] = [l.replace('location ', '') for l in test['location']]
+test_merged = test.join(resourceTypeVectorized).join(eventTypeVectorized).join(logFeatureVectorized).join(severityType)
+
+neighbors = featured_neighbors()
+train_merged = train_merged.join(neighbors)
+test_merged = test_merged.join(neighbors)
+
+# save
+train_merged.to_csv(join(data_path, 'train_merged.csv'))
+test_merged.to_csv(join(data_path, 'test_merged.csv'))
---
+++ predict_model_xgboost.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from os.path import join
+from sklearn.cross_validation import train_test_split
+from sklearn import manifold
+
+data_path = r'/Major 1/data/'
+
+def preprocess(df):
+    f = [c for c in df.columns if not c in['fault_severity', 'tsne0', 'tsne1']]
+    df[f] = np.sqrt(df[f]+3.0/8.0)
+    return df
+
+
+#t-Distributed Stochastic Neighbor Embedding for dimentionality reduction
+def add_tsne_features(dtrain, dtest):
+
+    tmp = dtrain.append(dtest)
+    tmp = tmp.reindex_axis(dtrain.columns, axis=1) # reorder columns again
+    features = tmp.columns.drop(['id', 'fault_severity'])[:39]
+    features = list(features) + ['severity_type']
+    tmp=tmp[features].groupby('location').mean()
+
+    tsne = manifold.TSNE(n_components=2, verbose=2, n_iter=500, perplexity=30)
+    X_2d = tsne.fit_transform(tmp.as_matrix())
+    
+    tmp = pd.DataFrame(tmp.reset_index(), columns=('location', 'tsne0', 'tsne1'))
+    tmp[['tsne0', 'tsne1']] = X_2d
+    
+    dtrain = dtrain.merge(tmp, on='location', sort=False, how='left') 
+    dtest = dtest.merge(tmp, on='location', sort=False, how='left')   
+    
+    return dtrain, dtest
+
+test = pd.read_csv(join(data_path, 'test_merged.csv'))
+train = pd.read_csv(join(data_path, 'train_merged.csv'))
+
+train, test = add_tsne_features(train, test)
+
+features = train.columns.drop(['id', 'fault_severity', 'location'])
+
+mode = 'fulltrain'  # validate|fulltrain
+n_classes = 3
+prepr = True
+
+params = {'objective': 'multi:softprob',
+         'eval_metric': ['merror', 'mlogloss'],
+         'num_class': n_classes,
+         'max_depth': 5,
+         'eta': 0.05,
+         'sub_sample': 0.7,
+         'colsample_bytree': 0.4,
+         'min_child_weight': 0,
+         'seed': 1,
+         'silent': 1}
+
+if prepr:
+    train = preprocess(train)
+
+X = train[features].values
+y = train['fault_severity'].values
+
+if mode == 'validate':
+    print("Validating...")
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.1,
+                                                        random_state=15,
+                                                        stratify=y)
+    dtrain = xgb.DMatrix(X_train, label=y_train)
+    dtest = xgb.DMatrix(X_test, label=y_test)
+    watchlist  = [(dtrain,'train'), (dtest,'eval')]
+
+    num_round = 10000
+    bst = xgb.train(params, dtrain, num_round, watchlist,
+                                    early_stopping_rounds=1000)
+elif mode == 'fulltrain':
+    print("Full training...")
+
+    dtrain = xgb.DMatrix(X, label=y)
+    watchlist  = [(dtrain,'train')]
+    
+    num_round = 1126
+    bst = xgb.train(params, dtrain, num_round, watchlist)    
+    
+    print("Predicting")
+    if prepr:
+        test = preprocess(test)
+    X_test = test[features].values
+    ypred = bst.predict(xgb.DMatrix(X_test)).reshape(-1, n_classes)
+    
+    result = pd.read_csv(join(data_path, 'sample_result.csv'))
+    cols = ['predict_0', 'predict_1', 'predict_2']
+    result[cols] = ypred
+    result.to_csv(join(data_path, 'results',
+                           'result.csv'), index=False)
+else:
+    print("Usupported mode")
